ad7dcb778809eeb59e568b4f92e74c553970f4d9
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-03-30 11:32:28 +0000

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HouseComponent } from './components/house/house.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HouseComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
