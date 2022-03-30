b2520cd2482d9217d423a02c6f3857602b6ff842
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-03-30 11:29:57 +0000

    add house model

---
+++ src/app/models/house.model.ts
@@ -0,0 +1,10 @@
+import { Floor } from "./floor.model";
+import { User } from "./user.model";
+
+export interface House {
+    id: number,
+    name: string,
+    adress: string,
+    user: User,
+    floors: Floor[]
+}
