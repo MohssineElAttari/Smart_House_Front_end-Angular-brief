13d4e31098e11c60d0e96918ca26067fb4ea6c92
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-03-31 20:11:21 +0000

    new update in house componenet

--- src/app/components/house/house.component.ts
+++ src/app/components/house/house.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { HouseService } from '../../services/house.service';
+import { House } from '../../models/house.model';
+import { User } from 'src/app/models/user.model';
+import { Floor } from 'src/app/models/floor.model';
+import { Room } from 'src/app/models/room.model';
+import { Device } from 'src/app/models/device.model';
+import { DeviceService } from 'src/app/services/device.service';
 
 @Component({
   selector: 'app-house',
@@ -7,9 +14,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HouseComponent implements OnInit {
 
-  constructor() { }
+  constructor(private deviceService: DeviceService) { }
 
   ngOnInit(): void {
+    this.GetAllDevices();
+
   }
+  devices: any = []
+  houses: House[] = [];
+  user: User[] = [];
+  floors: Floor[] = [];
+  rooms: Room[] = [];
+  /**
+   * GetAllHouses
+   */
+  public GetAllDevices() {
+    return this.deviceService.findAll().
+      subscribe((device) => {
+        this.devices = device;
+        console.log(this.devices);
+      });
+  }
+
 
 }
